608f06281d68a47f52b39c0202a86f2b8deb5926
Tom Zaki <zakatk857@gmail.com>
2013-02-04 05:01:55 -0500

    organized code, added comments, and turned off php error reporting

--- php/resistortool.php
+++ php/resistortool.php
@@ -1,34 +1,67 @@
 <?php
-   
-   $gain = isset($_POST['gain']) ? $_POST['gain'] : 0;  
+// Given POST data for the following variables, this code will calculate
+//   a set of optimal resistor choices for a desired gain, taking into
+//   account a handful of typical contraints:
+//
+//    POST     Name        Description
+//  ----------------------------------------------------------------------------------
+//  - gain     Gain        Desired DC gain from resistor combination 
+//  - thresh   Threshold   Acceptable error range expressed as a percent of nominal gain 
+//  - r1l      Res1 Low    Lowest acceptable value for feedback resistor 
+//  - r1h      Res1 High   Highest acceptable value for feedback resistor
+//  - tol      Tolerance   Resistor Tolerance (typical values between 20%-0.5%)
+//  - inv      Inverting   Feedback configuration: inverting or non-inverting
+//
 
-   function cmp($a, $b) {
-      if ($a[0] == $b[0]) {
-         if ($a[1] == $b[1]) {
-            return 0;
-         }
-         return ($a[1] < $b[1]) ? -1 : 1;
-      }
-      return ($a[0] < $b[0]) ? -1 : 1;
-   }
-   
-   function d_cmp($a, $b) {
-      $gain = isset($_POST['gain']) ? $_POST['gain'] : 0; 
-      if(dist_cmp($a[0], $b[0], $gain) == 0){
-         return ($a[1] < $b[1]) ? -1 : 1;
-      }
-      return dist_cmp($a[0], $b[0], $gain);
-   }
-   
-   function dist_cmp($a, $b, $loc){   
-      $d_a = abs($a - $loc);  
-      $d_b = abs($b - $loc);
-      if($d_a == $d_b) return 0;
-      return ($d_a < $d_b) ? -1 : 1;   
-   }
+   // Check if gain is set. If it's not then theres no point
+   // going through the rest of the script.
+   $gain = isset($_POST['gain']) ? $_POST['gain'] : 0;    
 
    if($gain != 0){
-   
+      
+      // Basic compare function for PHP's usort()
+      //  - Sorting precedence: Gain, R1, R2
+      //  - No longer used, but is still a valid alternative
+      //    to the current sorting method, d_cmp()
+      //
+      //function cmp($a, $b) {
+      //   if ($a[0] == $b[0]) {
+      //      if ($a[1] == $b[1]) {
+      //         return 0;
+      //      }
+      //      return ($a[1] < $b[1]) ? -1 : 1;
+      //   }
+      //   return ($a[0] < $b[0]) ? -1 : 1;
+      //}
+      
+      // More sophisticated sorting method based on delta
+      // between nominal gain and real gain
+      //  - Sorting precedence: Delta gain, R1
+      //
+      function d_cmp($a, $b) {
+         $gain = isset($_POST['gain']) ? $_POST['gain'] : 0; 
+         if(dist_cmp($a[0], $b[0], $gain) == 0){
+            return ($a[1] < $b[1]) ? -1 : 1;
+         }
+         return dist_cmp($a[0], $b[0], $gain);
+      }
+      
+      // Helper method for d_cmp()
+      //   - usort() compare methods can only take two
+      //     parameters so this method is called from
+      //     d_cmp() to allow for a third argument
+      //      
+      function dist_cmp($a, $b, $loc){   
+         $d_a = abs($a - $loc);  
+         $d_b = abs($b - $loc);
+         if($d_a == $d_b) return 0;
+         return ($d_a < $d_b) ? -1 : 1;   
+      }
+      
+      // Define resistor values. This could be done
+      // dynamically, but with such a large amount 
+      // of data and nearly unlimited memory on the
+      // server, this is much more efficient.
       $e192 = array(1.00,1.01,1.02,1.04,1.05,1.06,1.07,1.09,1.10,1.11,1.13,1.14,1.15,1.17,1.18,1.20,
                     1.21,1.23,1.24,1.26,1.27,1.29,1.30,1.32,1.33,1.35,1.37,1.38,1.40,1.42,1.43,1.45,
                     1.47,1.49,1.50,1.52,1.54,1.56,1.58,1.60,1.62,1.64,1.65,1.67,1.69,1.72,1.74,1.76,
@@ -54,22 +87,11 @@
                     4.70,5.10,5.60,6.20,6.80,7.50,8.20,9.10);
       $e012 = array(1.00,1.2,1.5,1.8,2.2,2.7,3.3,3.9,4.7,5.6,6.8,8.2);
       $e006 = array(1.00,1.5,2.2,3.3,4.7,6.8);
-
-      $arr  = array();
-      $good = array();
-      $pow = 0; // keeps track of divisions so the result can be multiplied at the end
       
-      $inv  = isset($_POST['inv'])  ? $_POST['inv']  : 1;
-      $gain_inv = $inv;
-      while($gain >= 10) {
-         $gain /= 10;
-         $gain_inv /= 10;
-         $pow++;
-      }
-      $thresh = $_POST['thresh']/100*$gain;
+      // arr will be assigned to one of the previously
+      // generated arrays based on tol      
+      $arr  = array();   
       $tolerance = $_POST['tol'];
-      $r1_low = $_POST['r1l'];
-      $r1_high = $_POST['r1h'];
       switch ($tolerance){
          case 'E6':   $arr = $e006; break;
          case 'E12':  $arr = $e012; break;
@@ -77,8 +99,29 @@
          case 'E48':  $arr = $e048; break;
          case 'E96':  $arr = $e096; break;
          case 'E192': $arr = $e192; break;
-      }                  
+      }    
       
+      
+      // keeps track of divisions so that the calculated 
+      // gain can be multiplied back to the right order 
+      // of magnitude at the end
+      $pow = 0;
+      $inv  = isset($_POST['inv'])  ? $_POST['inv']  : 1; // assume non-inverting if not set
+      $gain_inv = $inv;
+      while($gain >= 10) {
+         $gain /= 10;
+         $gain_inv /= 10;
+         $pow++;
+      }
+      
+      // good will store all of the successful resistor combinations
+      // $$$ todo: optimize the loops below. They're already very fast
+      //           becasue of the limited data that needs to be processed
+      //           but they could be *MUCH* faster
+      $thresh = $_POST['thresh']/100*$gain;
+      $r1_low = $_POST['r1l'];
+      $r1_high = $_POST['r1h'];                   
+      $good = array();
       foreach ($arr as $i => $value_i){
          foreach ($arr as $j => $value_j) {
             //echo("r1: ".$value_i." r2: ".$value_j." gain: ".($value_i/$value_j + $gain_inv)."<br>");
@@ -90,6 +133,10 @@
             }
          }
       }
+      
+      // If there were successful resistor combinations, draw 
+      // a table with those values. If not, print failure
+      // message in its place.
       if(count($good) > 0) {
          usort($good, "d_cmp");
          echo("<table class='bottomBorder'>    
@@ -108,17 +155,19 @@
                      <th style='text-align: left'>R2</th>
                   </tr>");          
          foreach($good as $index => $ans){
-            if($r1_high >= $ans[1] && $r1_low <= $ans[1]){
-               echo("<tr><td>". round(-pow(-1,$inv)*$ans[0],4).
-                    "</td><td>".round(-pow(-1,$inv)*20*log($ans[0],10),4).
-                    "</td><td>".round(abs($gain*pow(10, $pow) - $ans[0]),4).
-                    "</td><td>".$ans[1]."</td><td>".$ans[2]."</td></tr>");
+            if($r1_high >= $ans[1] && $r1_low <= $ans[1]){   // check r1 range, print:
+               echo("<tr><td>". round(-pow(-1,$inv)*$ans[0],4).              // gain
+                    "</td><td>".round(-pow(-1,$inv)*20*log($ans[0],10),4).   // r1
+                    "</td><td>".round(abs($gain*pow(10, $pow) - $ans[0]),4). // r2
+                    "</td><td>".$ans[1]."</td><td>".$ans[2]."</td></tr>");   // end row
             }
          }
          
          echo("</table>");
       } else {
-         echo("No combinations meet a gain of <b><span style='color: #993300;'>".$gain*pow(10, $pow)."</span></b> within the specified threshold using <b><span style='color: #993300;'>".$tolerance."</span></b> resistors");
+         echo("No combinations meet a gain of <b><span style='color: #993300;'>".$gain*pow(10, $pow).
+              "</span></b> within the specified threshold using <b><span style='color: #993300;'>".
+              $tolerance."</span></b> resistors");
       }
    }
 ?>
