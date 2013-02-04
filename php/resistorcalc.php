9970b3f45b4b22942eb62a8ef62671786573cb72
Tom Zaki <zakatk857@gmail.com>
2013-02-04 00:16:44 -0500

    added new CSS to support two-column layout, support for inverting and non-inverting gains in resistor gain tool

--- php/resistorcalc.php
+++ php/resistorcalc.php
@@ -16,7 +16,12 @@
 
    <body>
       <?php include($dir_inc."header.php"); ?>
-      <div id="content">
+      <div id="content_left">         
+         <div id="result">
+            <?php include("resistortool.php"); ?>
+         </div>
+      </div>
+      <div id="content_right">
          <table style="box-shadow: none;">
             <colgroup>
                <col span="1" style="width: 250px;">
@@ -25,12 +30,17 @@
                <form method="post">
                
                <?php 
-                  $gain   = isset($_POST['gain'])   ? $_POST['gain']   : ""; 
+                  $gain   = isset($_POST['gain'])   ? $_POST['gain']   : "100"; 
                   $thresh = isset($_POST['thresh']) ? $_POST['thresh'] : "5";
                   $r1l    = isset($_POST['r1l'])    ? $_POST['r1l']    : "0";
                   $r1h    = isset($_POST['r1h'])    ? $_POST['r1h']    : "999";
                   $tol    = isset($_POST['tol'])    ? $_POST['tol']    : "E48";
+                  $inv    = isset($_POST['inv'])    ? $_POST['inv']    : 1;
                   echo("
+                  Type:<br><select type='text' name='inv'>
+                     <option value=1".(($inv == 1) ? " selected" : "").">Non-Inverting</option>
+                     <option value=0".(($inv == 0) ? " selected" : "").">Inverting</option>
+                  </select><br><br>
                   Gain:<br>              <input  type='text' name='gain'   value=$gain><br><br>
                   Gain Threshold(%):<br> <input  type='text' name='thresh' value=$thresh><br><br>
                   R1 Low:<br>            <input  type='text' name='r1l'    value=$r1l><br><br>
@@ -46,13 +56,14 @@
                   </select><br><br>
                   ");
                ?>
-                  <input type="submit" value="Calculate" onclick="calc()">
+                  <input type="submit" value="Calculate" onclick="calc()"><br>
+                  <p style="font-size: 75%">                  
+                     Note: There is a known sorting bug in this applicaiton. The results are first sorted by
+                     &Delta; and then by R1. The sorting is <i>almost</i> perfect, but for some reason
+                     the last item in the list is always out of place. As far as I can tell this is a
+                     bug with php5's usort function and not my &Delta; comparison function.
+                  </p>
                </form>
-            </td><td style="vertical-align: top;">
-               <div id="result">
-                     <?php include("resistortool.php"); ?>
-               </div>
-            </td></tr>
          </table>
       </div>
       <?php include($dir_inc."footer.php"); ?>
